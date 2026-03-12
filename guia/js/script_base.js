857db88166772286dcbde9f7ee6352d6cb7ac75f
Miquel Marquès <miquelmb11@gmail.com>
2026-03-12 18:19:01 +0100

    Update guia Millora de la guia de base

---
+++ guia/js/script_base.js
@@ -0,0 +1,9 @@
+function NavMostrar() {
+    var NavSubLlista = document.getElementById("SubLlista")
+    if (NavSubLlista.style.display == "none") {
+        NavSubLlista.style.display = "block";
+    }
+    else {
+         NavSubLlista.style.display = "none";
+    }
+}
