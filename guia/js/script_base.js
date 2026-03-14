5e672c69e1dc6ffb64490906000f60bdf9a10cb9
Miquel Marquès <miquelmb11@gmail.com>
2026-03-14 11:36:24 +0100

    Update guia folder Creat un nou aparatat de HTML basic amb funció d'executar.

--- guia/js/script_base.js
+++ guia/js/script_base.js
@@ -7,3 +7,18 @@ function NavMostrar() {
          NavSubLlista.style.display = "none";
     }
 }
+function MostrarCommentaris() { // Arreglar Funció NO FUNCIONA
+    var Comentari = document.getElementsByClassName('comment');
+        if (Comentari.style.display == "none") {
+        Comentari.style.display = "block";
+    }
+    else {
+         Comentari.style.display = "none";
+    }
+}
+function ExecutarCodiHTML(boto){
+    const bloc = boto.parentElement;                
+    const codi = bloc.querySelector('.codi').value;  
+    const resultat = bloc.querySelector('.resultat'); 
+    resultat.innerHTML = codi; 
+}
