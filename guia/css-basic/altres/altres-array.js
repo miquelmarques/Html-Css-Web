b172a963c322946e33b4b1ccae7ba4eab0e7a38c
Miquel Marquès <miquelmb11@gmail.com>
2026-03-16 15:37:21 +0100

    Update Altres Propietats S'ha fet una millora de cerca de propietats a traves de fer un "laboratori de proves" funcionant amb JS.

---
+++ guia/css-basic/altres/altres-array.js
@@ -0,0 +1,174 @@
+const PropietatsAmbDependenciesCSS = [
+    {
+        "name": "background-attachment",
+        "codi": " background-image:url('imatges/fons.jpg');\nbackground-attachment:fixed;\nbackground-repeat: no-repeat;\nbackground-size:cover;\n",
+
+    },
+    {
+        "name": "background",
+        "codi": " background: url('imatges/fons.jpg) fixed cover;\n",
+
+    },
+    {
+        "name": "margin-top / margin-left / margin-bottom / margin-right",
+        "codi": "margin-top:10px;\n margin-left:25px;\n margin-bottom:0px;\n margin-right:5px;\n",
+    },
+    {
+        "name": "margin",
+        "codi": "margin: 10px 5px;\n",
+    },
+    {
+        "name": "padding-top / padding-left / padding-bottom / padding-right",
+        "codi": "padding-top:10px;\n padding-left:25px;\n padding-bottom:0px;\n padding-right:5px;\n",
+    },
+    {
+        "name": "padding",
+        "codi": "padding: 10px 5px;\n",
+    },
+    {
+        "name": "border-top-width / border-bottom-width / border-left-width / border-right-width",
+        "codi": "border-top-width:4px;\n border-bottom-width:10px;\n border-left-width:1px;\n border-right-width:5px;\nborder-style: solid;\nborder-color:red;\n",
+    },
+    {
+        "name": "border-width",
+        "codi": "border-width: 5px 10px;\nborder-style: solid;\nborder-color:red;\n",
+    },
+    {
+        "name": "border-top-style / border-bottom-style / border-left-style / border-right-style",
+        "codi": "border-top-style:dashed;\n border-bottom-style:solid;\n border-left-style:dotted;\n border-right-style:double;\nborder-width:5px;\nborder-color:red;\n",
+    },
+    {
+        "name": "border-style",
+        "codi": "border-style: double;\nborder-width:5px;\nborder-color:red;\n",
+    },
+    {
+        "name": "border-top-color / border-bottom-color / border-left-color / border-right-color",
+        "codi": "border-top-color:red;\n border-bottom-color:blue;\n border-left-color:green;\n border-right-color:yellow;\nborder-width:5px;\nborder-style:solid;\n",
+    },
+    {
+        "name": "border-color",
+        "codi": "border-color: gray green;\n",
+    },
+    {
+        "name": "border-top / border-bottom / border-left / border-right",
+        "codi": "border-top: solid blue 4px;\n border-left: dashed black 2px;\n",
+    },
+    {
+        "name": "border",
+        "codi": "border: 4px solid yellow;\n",
+    },
+    {
+        "name": "border-top-left-radius / border-top-right-radius / border-bottom-left-radius / border-bottom-right-radius",
+        "codi": "border-top-left-radius:5px;\n border-top-right-radius:10px;\n border-bottom-left-radius:0px;\n border-bottom-right-radius:50px;\n border:solid red 2px;\n",
+    },
+    {
+        "name": "border-radius",
+        "codi": "border-radius:10px;\nborder:solid red 2px;\n",
+    },
+    {
+        "name": "box-shadow",
+        "codi": "box-shadow: 10px 5px 5px red;\n",
+    },
+    {
+        "name": "border-spacing",
+        "codi": "border-spacing:10px;\n",
+    },
+    {
+        "name": "box-sizing",
+        "codi": "box-sizing:border-box;\n width:40px;\n",
+    },
+    {
+        "name": "list-style-type",
+        "codi": "list-style-type: upper-roman;\n",
+    },
+    {
+        "name": "list-style-image",
+        "codi": "list-style-image: url(../images/[imatge]);\n",
+    },
+    {
+        "name": "list-style-position",
+        "codi": "list-style-position: inside;\n",
+    },
+    {
+        "name": "list-style",
+        "codi": "list-style: square inside;\n",
+    },
+    {
+        "name": "width / height",
+
+        "codi": "width:50px;\n",
+    },
+    {
+        "name": "max-width / max-height",
+
+        "codi": "max-height:200px;\n",
+    },
+    {
+        "name": "min-width / min-height",
+        "codi": "min-width:500px;\n min-height:100px;\n",
+    },
+    {
+        "name": "cursor",
+        "codi": "cursor: wait;\n",
+    },
+    {
+        "name": "visibility",
+        "codi": "visibility: hidden;\n",
+    },
+    {
+        "name": "display",
+        "codi": "display: inline;\n",
+    },
+    {
+        "name": "opacity",
+        "codi": "opacity:0.5;\n",
+    },
+    {
+        "name": "overflow",
+        "codi": "overflow:hidden;\n",
+    },
+    {
+        "name": "float",
+        "codi": "float:left;\n",
+    },
+    {
+        "name": "clear",
+        "codi": "clear:both;\n",
+    },
+    {
+        "name": "position",
+        "codi": "position:absolute;\n",
+    },
+    {
+        "name": "top / left / right / bottom",
+        "codi": "top:10px;\n left:10px;\n",
+    },
+    {
+        "name": "z-index",
+        "codi": "z-index:10;\n",
+    },
+    {
+        "name": "flex-direction",
+        "codi": "flex-direction: column-reverse;\n",
+    },
+    {
+        "name": "justify-content",
+        "codi": "justify-content: space-around;\n",
+    },
+    {
+        "name": "align-items / align-self",
+        "codi": "align-items: center;\n",
+    },
+    {
+        "name": "flex",
+        "codi": "flex: 1 1 auto;\n",
+    },
+    {
+        "name": "flex-wrap",
+        "codi": "flex-wrap: wrap;\n",
+    },
+    {
+        "name": "order",
+        "codi": "order: 2;\n",
+    }
+];
